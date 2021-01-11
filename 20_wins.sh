22677cb49c6c6d323c6c9d39bac1f3c9d5977946
MatthiasStierlin <mbstierlin@gmail.com>
2021-01-11 16:55:30 +0100

    Second version of the game

--- 20_wins.sh
+++ 20_wins.sh
@@ -3,123 +3,319 @@
 # Aufruf: 20_Gewinnt.sh
 # Matthias Stierlin
 
-#BASH Cases einbauen
-
-score=0
-round=1
+declare score=0
+declare round=1
+declare gameversion=''
+declare gamemode=1
 
 #Introduction
 echo "Welcome to 20 Gewinnt!"
 echo "20 Gewinnt! is a short mathematical game."
 echo
-echo "Choose your game mode"
-echo "1 = Player vs Player"
-echo "2 = Player vs Computer"
-echo "3 = Computer vs Computer (You are a spectator)"
-read gamemode
-echo
 
-announce_round () {
-        echo
-        tput setaf 1
-        echo Round nr. : $round
-        round=$(($round+1))
-        tput sgr0
+#
+get_GameVersion() {
+    tput setaf 7
+    echo "Choose your game version:"
+    echo "C = Classical"
+    echo "D = Developer's way :)"
+    echo "PC = Computer vs Computer (You are merely a spectator"
+    read gameversion
+    echo
+
+    case $gameversion in
+        # Classic
+        "C") get_Gamemode
+        classic
+        ;;
+
+        # The developer's version 
+        "D") get_Gamemode 
+        developer
+        ;;
+
+        # Computer vs Computer 
+        "PC") compVScomp
+        ;;
+
+        # Start again
+        *)
+            echo "Please give in one of the following options:"
+            get_GameVersion
+        ;;
+    esac
 }
 
-# Game-mode 1, Player vs Player
-if [ "$gamemode" -eq 1 ]; then
-    while [ "$score" -le 20 ]; do
-        
-        #Announce round
-        announce_round
+# Reads in the gamemode.
+get_Gamemode() {
+    echo "Choose your game mode:"
+    echo "1 = Player vs Player"
+    echo "2 = Player vs Computer"
+    read gamemode
+    echo 
+}
 
-        #Player 1 plays
-        echo Player 1 enter a 1 or 2:
-        read player1Input
-        echo
-        score=$(($score+$player1Input))
-        echo The score is right now: $score .
+# Prints out the round number.
+announce_round () {
+    echo
+    tput setaf 1
+    echo Round nr. : $round
+    round=$(($round+1))
+    tput sgr0
+}
 
-        #Check if Player 1 won.
-        if [ "$score" -eq 20  ] || [ "$score" -gt 20 ]; then
-            tput setaf 2
-            echo Player 1 won.
-            break
-        fi
-        echo
+# In this version of the game you play by
+# entering always the new score.
+classic() {
+      case $gamemode in
+        # Game-mode 1, Player vs Player
+        1) while true; do
+            #Announce round
+            announce_round
 
-        #Player 2 plays
-        echo Player 2 enter a 1 or 2:
-        read player2Input
-        echo
-        score=$(($score+$player2Input))
-        echo The score is right now: $score .
+            while [ "$score" -le 20 ]; do
+                #Player 1 plays
+                echo Player 1:
+                read player1Input
+                echo
 
-        #Check if Player 2 won.
-        if [ "$score" -eq 20 ] || [ "$score" -gt 20 ]; then
-            tput setaf 2
-            echo Player 2 won.
-            break
-        fi
-        echo
-    done
+                # Validate input
+                if [[ $player1Input -lt $(($score + 1)) ]] || [[ $player1Input -gt $(($score + 2)) ]]; then
+                    echo "Fehlerhafte Ausgabe"
+                    continue
+                elif [[ $player1Input -gt 20 ]]; then
+                    echo "Idiot, sag 20!"
+                    continue
+                else
+                    # Eingabe korrekt, weiter geht's
+                    break;
+                fi
+            done
+            
+            score=$player1Input
+            echo The score is right now: $score .
 
-# Game-mode 2, Player vs Computer
-elif [ "$gamemode" -eq 2 ]; then
-    while [ "$score" -le 20 ]; do
+            #Check if Player 1 won.
+            if [ "$score" -eq 20  ]; then
+                tput setaf 2
+                echo Player 1 won.
+                break
+            fi
+            echo
 
-        #Announce round
-        announce_round
+            while [ "$score" -le 20 ]; do
+                #Player 2 plays
+                echo Player 2:
+                read player2Input
+                echo
+            
+                # Validate input
+                if [[ $player2Input -lt $(($score + 1)) ]] || [[ $player2Input -gt $(($score + 2)) ]]; then
+                    echo "Wrong input"
+                    continue
+                elif [[ $player2Input -gt 20 ]]; then
+                    echo "Would you just say 20!"
+                    continue
+                else
+                    # Eingabe korrekt, weiter geht's
+                    break;
+                fi
+            done
 
-        #Player plays
-        echo Player enter a 1 or 2:
-        read player1Input
-        echo
-        score=$(($score+$player1Input))
-        echo The score is right now: $score .
+            score=$player2Input
+            echo The score is right now: $score .
 
-        #Check if Player won.
-        if [ "$score" -eq 20  ] || [ "$score" -gt 20 ]; then
-            tput setaf 2
-            echo Player won.
-            break
-        fi
-        echo
+            #Check if Player 2 won.
+            if [ "$score" -eq 20 ] || [ "$score" -gt 20 ]; then
+                tput setaf 2
+                echo Player 2 won.
+                break
+            fi
+            echo
+        done;;
 
-        #Computer plays
-        if [[ "$score" -eq "18" ]]; then
-            score=$(( $score + 2))
-            echo "Alpha - Computer: 2"
-        elif [[ "$score" -eq "19" ]]; then
-            score=$(( $score + 1))
-            echo "Alpha - Computer: 1"
-        else
-            echo "Computer:"
-            computerInput=$(($[RANDOM%2] + 1))
-            echo "Computer ${computerInput}"
-            score=$(( $score + $computerInput))
+        # Game-mode 2, Player vs Computer
+        2) while [ "$score" -le 20 ]; do
+
+            #Announce round
+            announce_round
+
+            while [ "$score" -le 20 ]; do
+                #Player 1 plays
+                echo Player 1:
+                read player1Input
+                echo
+
+                # Validate input
+                if [[ $player1Input -lt $(($score + 1)) ]] || [[ $player1Input -gt $(($score + 2)) ]]; then
+                    echo "Wrong input"
+                    continue
+                elif [[ $player1Input -gt 20 ]]; then
+                    echo "Would you just say 20!"
+                    continue
+                else
+                    # Eingabe korrekt, weiter geht's
+                    break;
+                fi
+            done
+            
+            score=$player1Input
+            echo "The score is right now: $score."
+
+            #Check if Player 1 won.
+            if [ "$score" -eq 20  ] || [ "$score" -gt 20 ]; then
+                tput setaf 2
+                echo Player 1 won.
+                break
+            fi
+            echo
+
+            #Computer plays
+            if [[ "$score" -eq "18" ]]; then
+                score=$(( $score + 2))
+                echo "2"
+            elif [[ "$score" -eq "19" ]]; then
+                score=$(( $score + 1))
+                echo "1"
+            else
+                echo "Computer:"
+                computerInput=$(($[RANDOM%2] + 1))
+                echo "${computerInput}"
+                score=$(( $score + $computerInput))
+            fi
             echo "The score is right now: ${score}."
-        fi
-        
-        #Check if Computer won.
-        if [ "$score" -eq 20 ] || [ "$score" -gt 20 ]; then
-            tput setaf 2
-            echo Computer won.
-            break
-        fi
-        echo
-    done
+            
+            #Check if Computer won.
+            if [ "$score" -eq 20 ] || [ "$score" -gt 20 ]; then
+                tput setaf 2
+                echo Computer won.
+                break
+            fi
+            echo
+        done;;
+    esac
+}
 
-# Game-mode 3, Computer vs Computer
-else
+# In this version of the game you play by
+# either entering 1 or 2.
+developer() {
+    case $gamemode in
+        # Game-mode 1, Player vs Player
+        1) while [ "$score" -le 20 ]; do
+            #Announce round
+            announce_round
+
+            while true; do
+                #Player 1 plays
+                echo Player 1 enter a 1 or 2:
+                read player1Input
+                echo
+
+                if [ "$player1Input" -eq 1 ] || [ "$player1Input" -eq 2 ]; then
+                    break
+                else 
+                    echo "Wrong input!"
+                fi
+            done
+
+            score=$(($score+$player1Input))
+            echo The score is right now: $score .
+
+            #Check if Player 1 won.
+            if [ "$score" -eq 20  ] || [ "$score" -gt 20 ]; then
+                tput setaf 2
+                echo Player 1 won.
+                break
+            fi
+            echo
+
+            while true; do
+                #Player 2 plays
+                echo Player 2 enter a 1 or 2:
+                read player2Input
+                echo
+
+                if [ "$player2Input" -eq 1 ] || [ "$player2Input" -eq 2 ]; then
+                    break
+                else 
+                    echo "Wrong input!"
+                fi
+            done
+
+            score=$(($score+$player2Input))
+            echo The score is right now: $score .
+
+            #Check if Player 2 won.
+            if [ "$score" -eq 20 ] || [ "$score" -gt 20 ]; then
+                tput setaf 2
+                echo Player 2 won.
+                break
+            fi
+            echo
+        done;;
+
+        # Game-mode 2, Player vs Computer
+        2) while [ "$score" -le 20 ]; do
+            #Announce round
+            announce_round
+
+            while true; do
+                #Player 1 plays
+                echo Player 1 enter a 1 or 2:
+                read player1Input
+                echo
+
+                if [ "$player1Input" -eq 1 ] || [ "$player1Input" -eq 2 ]; then
+                    break
+                else 
+                    echo "Wrong input!"
+                fi
+            done
+            
+            score=$(($score+$player1Input))
+            echo The score is right now: $score .
+
+            #Check if Player won.
+            if [ "$score" -eq 20  ] || [ "$score" -gt 20 ]; then
+                tput setaf 2
+                echo Player won.
+                break
+            fi
+            echo
+
+            #Computer plays
+            if [[ "$score" -eq "18" ]]; then
+                score=$(( $score + 2))
+                echo "Alpha - Computer: 2"
+            elif [[ "$score" -eq "19" ]]; then
+                score=$(( $score + 1))
+                echo "Alpha - Computer: 1"
+            else
+                echo "Computer:"
+                computerInput=$(($[RANDOM%2] + 1))
+                echo "Computer ${computerInput}"
+                score=$(( $score + $computerInput))
+                echo "The score is right now: ${score}."
+            fi
+            
+            #Check if Computer won.
+            if [ "$score" -eq 20 ] || [ "$score" -gt 20 ]; then
+                tput setaf 2
+                echo Computer won.
+                break
+            fi
+            echo
+        done;;
+    esac
+}
+
+compVScomp() {
+    # Computer vs Computer
     while [ "$score" -le 20 ]; do
-
         #Announce round
         announce_round
 
         #Alpha plays
-        echo
         if [[ "$score" -eq "18" ]]; then
             score=$(( $score + 2))
             echo "Alpha - Computer: 2"
@@ -130,14 +326,14 @@ else
             computerInput1=$(($[RANDOM%2] + 1))
             echo "Alpha - Computer: ${computerInput1}"
             score=$(( $score + $computerInput1))
-            echo "The score is right now: ${score}."
         fi
+        echo "The score is right now: ${score}."
 
         #Check if Alpha won.
-        if [ "$score" -eq 20 ] || [ "$score" -gt 20 ]; then
-                    tput setaf 2
-                    echo Alpha - Computer won.
-                    break
+        if [ "$score" -eq 20 ]; then
+            tput setaf 2
+            echo Alpha - Computer won.
+            break
         fi
 
         # Beta plays
@@ -152,19 +348,35 @@ else
             computerInput2=$(($[RANDOM%2] + 1))
             echo "Beta - Computer: ${computerInput2}"
             score=$(( $score + $computerInput2))
-            echo "The score is right now: ${score}."
         fi
+        echo "The score is right now: ${score}."
         
         #Check if Beta won.
-        if [ "$score" -eq 20 ] || [ "$score" -gt 20 ]; then
+        if [ "$score" -eq 20 ]; then
             tput setaf 2
             echo Beta - Computer won.
             break
         fi
-        
     done
-fi
+}
+
+while true; do
+    get_GameVersion
+
+    tput setaf 3
+    echo
+    echo "If you would like to play again press 'y'?"
+    read playAgain
+    if [[ "$playAgain" == "y" || "$playAgain" == "Y" ]]; then
+        score=0
+        round=1
+        echo
+        continue
+    else
+        break;
+    fi
+done
 
 echo
 tput setaf 2
-echo Auf Wiedersehen ☺!
+echo "Auf Wiedersehen ☺!"
